1544afbefb28da5bb75caa78afca83acb2da69df
Markus Hauhia <s2000579@edu.bc.fi>
2021-01-29 17:33:33 +0200

    add angular material package from npm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 import { AppComponent } from './app.component';
 import { LisaaTapahtumaComponent } from './lisaa-tapahtuma/lisaa-tapahtuma.component';
@@ -10,7 +11,8 @@ import { LisaaTapahtumaComponent } from './lisaa-tapahtuma/lisaa-tapahtuma.compo
     LisaaTapahtumaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
